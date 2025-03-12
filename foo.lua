cf14f5735e79fb1b8eb973c5631aaad596ccebeb
jeans <catbjeans@gmail.com>
2025-03-11 19:43:09 -0600

    chore: added loading file

---
+++ foo.lua
@@ -0,0 +1,9 @@
+-- This file will be excecuted before loading the called file or when running
+-- the lua interpreter
+-- This is achieved using the LUA_INIT environment variable
+-- export LUA_INIT="@foo.lua"
+-- if the '@' is not present, then the environment variable will threat it as 
+-- lua code e.g. LUA_INIT="print("loading this!")"
+print("--Loading foo.lua!!")
+x = 32
+
